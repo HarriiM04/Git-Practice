414445a8601bfc99b60a6d3c9bf845dad2ebda76
harrii <harrymakvana530@gmail.com>
2025-05-25 14:53:16 +0530

    added git ignore files

---
+++ db.php
@@ -0,0 +1,17 @@
+<?php
+$host = getenv('DB_HOST');
+$user = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+$dbname = getenv('DB_NAME');
+
+
+$conn = new mysqli($host, $user, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+else{
+    echo "Yess Connected with me !!!!!!";
+}
+?>
+
